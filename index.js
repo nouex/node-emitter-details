e87d578908c0df16ea2c2d5fcd228350a1495f62
Hudson Reece <alfaroamauri@gmail.com>
2015-07-03 12:04:01 -0400

    Add functionality to transfer _events to event details

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var EE = require("events");
 var util = require("util");
 var assert = require("assert");
 var Details = require("./lib/details.js");
+var common = require("./lib/common.js");
 
 /**
 * @api public
@@ -14,19 +15,54 @@ var Details = require("./lib/details.js");
 
 module.exports =
 function getDetails(emitter, opts) {
+  var doExit = null;
   var opts = opts || {};
+  if (!(opts.includeAutoEvents || util.isArray(opts.includeAutoEvents))) {
+    opts.includeAutoEvents = [false, false];
+  } else if (util.isArray(opts.includeAutoEvents)) {
+    assert.strictEqual(typeof opts.includeAutoEvents[0], "boolean", "`includeAutoEvents` el must be a boolean");
+    assert.strictEqual(typeof opts.includeAutoEvents[1], "boolean", "`includeAutoEvents` el must be a boolean");
+  }
+  opts.saveInactiveEventDetails = opts.saveInactiveEventDetails ? opts.saveInactiveEventDetails : false;
+
   assert.ok(emitter instanceof EE, "arg must be an Event Emitter");
 
   var details = new Details(emitter);
-
+  // NOTE the following line must go in this order
   emitter.on("newListener", onNewListener);
   emitter.on("removeListener", onRemoveListener);
+  // transfer normal events to detailed events
+  var _events = common.copy(emitter._events);
+  var exEvents = util.isArray(opts.excludedEvents) ? opts.excludedEvents : [];
+  opts.includeAutoEvents[0] ? void 0 : exEvents.push("newListener");
+  opts.includeAutoEvents[1] ? void 0 : exEvents.push("removeListener");
+  Object.keys(_events).forEach(function(name) {
+    var exEventFound;
+    if (!~exEvents.indexOf(name)) {
+      var single = false;
+      var count = util.isArray(_events[name]) ? _events[name].length : single = true;
+      single ? count = 1 : count = count;
+      for (var i = count; i; i--) {
+        single ? onNewListener(name, _events[name]) : onNewListener(name, _events[name][Math.abs(i - count)])
+      }
+    }
+  }, null);
+  _events = null;
 
   return details;
 
   function onNewListener(event, listener) {
     var eDetails;
     if (!(eDetails = details.getEventDetails(event))) {
+      // onRemoveListener with flag turned off
+      if (util.isNull(doExit)) {
+        opts.includeAutoEvents[1] === false ? doExit = true : doExit = false;
+        if (opts.includeAutoEvents[0] === true) {
+          onNewListener("newListener", onNewListener);
+        }
+        if (doExit)
+          return;
+      }
       details._addEvent(event, listener);
       eDetails = details.getEventDetails(event);
       if (!(event === "newListener" || event === "removeListener"))
@@ -63,5 +99,9 @@ function getDetails(emitter, opts) {
 // PROBE FIELD
 var opts = {
   saveInactiveEventDetails: false,
+  includeAutoEvents: [true, true],
+  exludedEvents: ["ename", "ename2"]
+};
 
-}
+// TODO
+// it is messy, fix it
---
+++ test/index/opts-includeAutoEvents.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var EE = require("events");
+var util = require("util");
+var getDetails = require("../../index.js");
+var assert = require("assert");
+var opts = {
+  saveInactiveEventDetails: true,
+  includeAutoEvents: [true, false]
+};
+
+function createEmitter() {
+  util.inherits(Class, EE);
+  function Class() {
+    EE.call(this);
+  }
+  return new Class
+}
+
+var e1 = createEmitter();
+var d1 = getDetails(e1, opts);
+
+// testing onNewListener
+function event1Dummy1() {console.log("EMITTED:  event1")}
+function event1Dummy2(a, b) {}
+function event2Dummy1() {}
+// registering events
+e1.on("event1", event1Dummy1);
+e1.on("event1", event1Dummy2);
+e1.on("event2", event2Dummy1);
+process.stdout.write(util.inspect(d1, {colors: true, depth: null}))
+process.exit(0);
+// removing events
+e1.removeListener("event1", event1Dummy1);
+e1.removeListener("event1", event1Dummy2);
+
+process.stdout.write(util.inspect(d1, {colors: true, depth: null}))
