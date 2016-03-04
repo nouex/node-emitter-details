2238a0997744879bc51befae76883ce6a329dfa3
Reece Hudson <reecehudson10@gmail.com>
2016-03-04 18:34:36 -0500

    test: added file

---
+++ test/event-details/getEmissionCxt().js
@@ -0,0 +1,10 @@
+"use strict";
+
+var assert = require("assert");
+var ED = require("../../lib/event-details.js");
+
+
+@index.js (added from index.js)
+  evDetails
+    name
+    genericEventRegulator
