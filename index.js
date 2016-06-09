24188d3c86a56e96367bfe849edbd33ddfeb1d8b
Reece Hudson <reecehudson10@gmail.com>
2016-06-09 15:09:38 -0400

    enable debug

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var EventDetails = require("./lib/event-details.js");
 var HandlerDetails = require("./lib/handler-details.js");
 var helpers = require("./lib/helpers.js");
 var getCallSite = require("./lib/stack-trace.js");
+var debug = require("debug")("node-emitter-details");
 
 /**
 * @api public
--- test.bat
+++ test.bat
@@ -1,5 +1,6 @@
 @ECHO OFF
 REM assuming CD is currently at root of project
+SET DEBUG=node-emitter-details
 CD ./test
 REM double quotes around %%f are needed cause the filename has a space
 FOR /r %%f IN (*) DO  ( ECHO NODE TESTING: & ECHO. %%f & ECHO ---
