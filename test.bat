f1f020006995a65bd2956ab9a717b76c9d87374c
Reece Hudson <reecehudson10@gmail.com>
2016-06-09 13:50:08 -0400

    fix: test.bat
    Return to the root dir when finished.

--- test.bat
+++ test.bat
@@ -4,6 +4,7 @@ CD ./test
 REM double quotes around %%f are needed cause the filename has a space
 FOR /r %%f IN (*) DO  ( ECHO NODE TESTING: & ECHO. %%f & ECHO ---
   node "%%f" )
+CD ../
 
 REM TODO use colors to output progress
 REM TODO exit automatically if not error
